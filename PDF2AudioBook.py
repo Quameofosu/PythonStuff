4a2e9fb6fdd36ad4a5c362837dfc37c0eb6af043
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-14 05:20:06 +0000

    Toggled Rate

--- PDF2AudioBook.py
+++ PDF2AudioBook.py
@@ -2,13 +2,13 @@ import PyPDF2
 import pyttsx3
 
 pdfReader = PyPDF2.PdfFileReader(open('PDF_file.pdf', 'rb'))
-engine = pyttsx3.init() #initialize the TTS engine
+engine = pyttsx3.init()  # initialize the TTS engine
 
 rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
-engine.setProperty('rate', 125)     # setting up new voice rate
-voices = engine.getProperty('voices')  #getting details of current voice
-engine.setProperty('voice', voices[1].id) #Changes voice to female
+print(rate)                         # printing current voice rate
+engine.setProperty('rate', 210)     # setting up new voice rate
+voices = engine.getProperty('voices')  # getting details of current voice
+engine.setProperty('voice', voices[1].id)  # Changes voice to female
 
 
 for page_num in range(pdfReader.numPages):
