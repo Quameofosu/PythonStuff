d089ad4afbf7f3fae0de5d016695ee7ca41f0dac
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-14 02:02:50 +0000

    Creata Adjective and Noun list

--- password_picker.py
+++ password_picker.py
@@ -1,3 +1,13 @@
 import random
 import string
+
+adjective = ['sleepy', 'slow', 'smelly', 'wet', 'fat',
+             'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'fluffy',
+             'white', 'proud', 'brave']
+
+nouns = ['apple', 'dinosaur', 'ball',
+         'toaster', 'goat', 'dragon',
+         'hammer', 'duck', 'panda']
+
+
 print("Welcome to Password Picker!")
