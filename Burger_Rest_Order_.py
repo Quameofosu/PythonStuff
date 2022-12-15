5714d568bb956874bc336599eb6e19f825b92f70
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-15 21:34:11 +0000

    Still tryinig to fix the code

--- Burger_Rest_Order_.py
+++ Burger_Rest_Order_.py
@@ -1,19 +1,19 @@
 print("Welcome to Grillby’s Burger Restaurant ")
 burger_options = ['Regular', 'Glutten-free', 'Veggie',
                   'Impossible burger', 'Royale', 'Vegan-Only']
-print("----------This is our Menu----------")
-print("1. Regular\n2. Glutten-free\n3. Veggie\n4. Impossible burger\n5. Royale\n6. Vegan-Only")
-
-i = 0
 answer_num = [1, 2, 3, 4, 5, 6]
+
+print("----------This is our Menu----------")
+print("1. Regular\n2. Glutten-free\n3. Veggie\n4. Impossible burger\n5. Royale\n6. Vegan-Only\n\nType Numbers Only")
 
 user_input = input(
     'What kind of Burger and burn do you want?\nYour Answer: ')
 
-while (i < 6):
-    if answer_num or burger_options[i]:
+i = 0
+while (i < 3):
+    if user_input == answer_num[i]:
         print("You chose ", user_input)
-        break
+        i = i + 1
     else:
         print("Please enter a valid option")
         user_input = input(
