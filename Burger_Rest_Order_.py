2dd53c56fa8eacf291d237c74d60f2b8bd5d3a7f
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-15 19:45:18 +0000

    Created Burger_Restaurant_order code

---
+++ Burger_Rest_Order_.py
@@ -0,0 +1,12 @@
+print("Welcome to Grillby’s Burger Restaurant ")
+burger_options = ['Regular', 'Glutten-free', 'Veggie',
+                  'Impossible burger', 'Royale', 'Vegan-Only']
+print("----------This is our Menu----------")
+print("1. Regular\n2. Glutten-free\n3. Veggie\n4. Impossible burger\n5. Royale\n6. Vegan-Only")
+
+answer_num = [1, 2, 3, 4, 5, 6]
+while True:
+    user_input = input(
+        'What kind of Burger and burn do you want?\nYour Answer: ')
+    continue
+print
