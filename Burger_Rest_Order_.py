e07301e7dca02ada6033b8d703cd4e75cd13f7e0
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-15 20:14:15 +0000

    Trying to fix loop problem

--- Burger_Rest_Order_.py
+++ Burger_Rest_Order_.py
@@ -4,9 +4,20 @@ burger_options = ['Regular', 'Glutten-free', 'Veggie',
 print("----------This is our Menu----------")
 print("1. Regular\n2. Glutten-free\n3. Veggie\n4. Impossible burger\n5. Royale\n6. Vegan-Only")
 
+i = 0
 answer_num = [1, 2, 3, 4, 5, 6]
-while True:
-    user_input = input(
-        'What kind of Burger and burn do you want?\nYour Answer: ')
+
+user_input = input(
+    'What kind of Burger and burn do you want?\nYour Answer: ')
+
+while (i < 6):
+    if answer_num or burger_options[i]:
+        print("You chose ", user_input)
+        break
+    else:
+        print("Please enter a valid option")
+        user_input = input(
+            'What kind of Burger and burn do you want?\nYour Answer: ')
     break
+
 print('Heelo Me')
