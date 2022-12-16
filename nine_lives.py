185dc9c0c243fc117d12e08a179ddc2d7fd55d6d
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-16 00:23:45 +0000

    Add words of varying length

--- nine_lives.py
+++ nine_lives.py
@@ -9,24 +9,33 @@ lives = 9
 words = ['pizza', 'fairy', 'teeth', 'shirt',
          'otter', 'plane', 'brush', 'horse', 'light']
 secret_word = random.choice(words)
-clue = list('?????')
+clue = []
+index = 0
+while index < len(secret_word):
+    clue.append('?')
+    index = index + 1
+
 heart_symbol = u'\u2764'
 guessed_word_correctly = False
 
 # Function to Update clue
 
+unknown_letters = len(secret_word)
 
-def update_clue(guessed_letter, secret_word, clue):
+
+def update_clue(guessed_letter, secret_word, clue, unknown_letters):
     index = 0
     while index < len(secret_word):
         if guessed_letter == secret_word[index]:
             clue[index] = guessed_letter
+            unknown_letters = unknown_letters - 1
         index = index + 1
+    return unknown_letters
 
 
 # Add difficult levels
 difficulty = input(
-    'Choose difficult (type 1, 2 or 3):\n 1. Easy\n 2. Normal\n 3. Hard\n')
+    'Choose difficult (type 1, 2 or 3):\n 1. Easy\n 2. Normal\n 3. Hard\n Your Answer: ')
 difficulty = int(difficulty)
 
 if difficulty == 1:
@@ -48,10 +57,14 @@ while lives > 0:
         break
 
     if guess in secret_word:
-        update_clue(guess, secret_word, clue)
+        unknown_letters = update_clue(
+            guess, secret_word, clue, unknown_letters)
     else:
         print('Incorrect. You lose a life')
         lives = lives - 1
+    if unknown_letters == 0:
+        guessed_word_correctly = True
+        break
 
 # Determine if player won or lost
 
