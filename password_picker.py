65a0e258d3934aa4234d8a020d95beac98041cbb
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-16 02:08:21 +0000

    Update password_picker.py

--- password_picker.py
+++ password_picker.py
@@ -36,12 +36,11 @@ while True:
 
 # QR code generator
 
+    # String which represent the QR code
+    #s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
 
-# String which represent the QR code
-s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
+    # Generate QR code
+    #url = pyqrcode.create(s)
 
-# Generate QR code
-url = pyqrcode.create(s)
-
-# Create and save the png file naming "myqr.png"
-url.svg("myyoutube.svg", scale=8)
+    # Create and save the png file naming "myqr.png"
+    #url.svg("myyoutube.svg", scale=8)
