03ee9f53dac14e3e7ed6320f96acb28e047b7de8
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-17 20:02:40 +0000

    Update Robot_builder.py

--- Burger_Rest_Order_.py
+++ Burger_Rest_Order_.py
@@ -6,18 +6,15 @@ answer_num = [1, 2, 3, 4, 5, 6]
 print("----------This is our Menu----------")
 print("1. Regular\n2. Glutten-free\n3. Veggie\n4. Impossible burger\n5. Royale\n6. Vegan-Only\n\nType Numbers Only")
 
-user_input = input(
-    'What kind of Burger and burn do you want?\nYour Answer: ')
+i = 1
 
-i = 0
-while (i < 3):
+while (i < 7):
+    user_input = input(
+        'What kind of Burger and burn do you want?\nYour Answer: ')
     if user_input == answer_num[i]:
-        print("You chose ", user_input)
-        i = i + 1
+        print("You selected {}".format(user_input))
     else:
         print("Please enter a valid option")
-        user_input = input(
-            'What kind of Burger and burn do you want?\nYour Answer: ')
-    break
+    i += i
 
 print('Heelo Me')
--- robot_builder.py
+++ robot_builder.py
@@ -15,6 +15,26 @@ def rectangle(horizontal, vertical, color):
     t.penup()
 
 
+def arm(color):
+    t.pendown()
+    t.begin_fill()
+    t.color(color)
+    t.forward(60)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(10)
+    t.right(90)
+    t.forward(40)
+    t.left(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(10)
+    t.end_fill()
+    t.penup()
+    t.setheading(0)
+
+
 t.penup()
 t.speed('slow')
 t.bgcolor('orange')
@@ -26,7 +46,7 @@ t.goto(-30, -150)
 rectangle(50, 20, 'blue')
 
 # legs
-t.goto(-25, 50)
+t.goto(-25, -50)
 rectangle(15, 100, 'grey')
 t.goto(-55, -50)
 rectangle(-15, 100, 'grey')
@@ -36,9 +56,9 @@ t.goto(-90, 100)
 rectangle(100, 150, 'red')
 
 # arms
-t.goto(–150, 70)
+t.goto(-150, 70)
 rectangle(60, 15, 'grey')
-t.goto(–150, 110)
+t.goto(-150, 110)
 rectangle(15, 40, 'grey')
 t.goto(10, 70)
 rectangle(60, 15, 'grey')
@@ -46,19 +66,19 @@ t.goto(55, 110)
 rectangle(15, 40, 'grey')
 
 # neck
-t.goto(–50, 120)
+t.goto(-50, 120)
 rectangle(15, 20, 'grey')
 
 # head
-t.goto(–85, 170)
+t.goto(-85, 170)
 rectangle(80, 50, 'red')
 
 # eyes
-t.goto(–60, 160)
+t.goto(-60, 160)
 rectangle(30, 10, 'white')
-t.goto(–55, 155)
+t.goto(-55, 155)
 rectangle(5, 5, 'black')
-t.goto(–40, 155)
+t.goto(-40, 155)
 rectangle(5, 5, 'black')
 
 # mouth
