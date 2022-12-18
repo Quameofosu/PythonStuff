708100eec50a23dd94128759ca829d06481dd5f7
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-18 16:05:32 +0000

    Create First_mqtt.py

---
+++ First_mqtt.py
@@ -0,0 +1,8 @@
+import paho.mqtt.client as mqtt
+
+# Set up the MQTT client
+client = mqtt.Client()
+client.connect("localhost", 1883, 60)
+
+# Publish a message with the topic "bike/sensor/accelerometer"
+client.publish("bike/sensor/accelerometer", "Accelerometer data")
