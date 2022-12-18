767de8da7c3edd331bdd44dec3eebed42a8f5d47
Manuel Ofosu <quameofosuemma@gmail.com>
2022-12-18 01:25:52 +0000

    Update Burger_Rest_Order.py

--- Burger_Rest_Order_.py
+++ Burger_Rest_Order_.py
@@ -6,15 +6,17 @@ answer_num = [1, 2, 3, 4, 5, 6]
 print("----------This is our Menu----------")
 print("1. Regular\n2. Glutten-free\n3. Veggie\n4. Impossible burger\n5. Royale\n6. Vegan-Only\n\nType Numbers Only")
 
-i = 1
 
-while (i < 7):
-    user_input = input(
+user_input = input(
         'What kind of Burger and burn do you want?\nYour Answer: ')
-    if user_input == answer_num[i]:
-        print("You selected {}".format(user_input))
-    else:
-        print("Please enter a valid option")
-    i += i
 
-print('Heelo Me')
+while True:
+    try:
+        if int(user_input) in answer_num:
+            print("You selected {}".format(user_input))
+        break
+    except ValueError:
+        print("Please Enter a Number")
+
+
+print('Heello Me')
